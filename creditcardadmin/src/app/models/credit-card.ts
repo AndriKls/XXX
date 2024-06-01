654f919601f632caeb8a96e07d8644623e6d83ee
AndriKls <158137294+AndriKls@users.noreply.github.com>
2024-06-01 20:11:11 +0300

    Comment1

---
+++ creditcardadmin/src/app/models/credit-card.ts
@@ -0,0 +1,14 @@
+export interface Creditcard {
+    id: number;
+    name: string;
+    description: string;
+    bankName: string;
+    maxCredit: number;
+    interestRate: string;
+    active: boolean;
+    recommendedScore: string;
+    annualFee: number;
+    termsAndConditions: string;
+    createdDate: string;
+    updatedDate: string
+}
